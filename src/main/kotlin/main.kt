7ae7feec8a1ea5d48b28cababc2c6a1f462c151b
Tiago <tiagoadriano9358@gmail.com>
2023-03-24 10:56:46 +0000

    b

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,50 +1,51 @@
 import isel.leic.UsbPort
 
-    fun main (args: Array<String>) {
-        /*while(true) {
-        val value = UsbPort.read()
-        UsbPort.write(value)
-    }
-    */
-       println(isBit(32))
+var output = 0
+
+fun main (args: Array<String>) {
+        init()
+
+            //val value = UsbPort.read()
+            //UsbPort.write(9)
+            //println(value)
+        setBits(9)
+        clrBits(1)
+
+
 
     }
-    fun readBits(mask: Int) : Int {
-            var num = mask
-            var decimalNumber = 0
-            var i = 0
-            var remainder: Long
-            var kappa = num.and(7)
-            println("lol = $kappa")
-            while (num != 0) {
-                remainder = num.toLong() % 10
-                num /= 10
-                decimalNumber += (remainder * Math.pow(2.0, i.toDouble())).toInt()
-                ++i
-            }
-            return decimalNumber
-        }
+
+fun init(){
+    UsbPort.write(0)
+}
+
 fun isBit(mask: Int): Boolean{
     val read = UsbPort.read()
     val kappa = mask.and(UsbPort.read())
     return kappa != 0
-
 }
-/*
 
-"10010001"
 
-        isBit("00000001")= true
-        isBit("10000000")= true
-        isBit("00000010")= false
-
- */
+// Retorna os valores dos bits representados por mask presentes no UsbPort
+fun readBits(mask: Int): Int{
+    val read = UsbPort.read()
+    return 1
+}
 
 fun writeBits(mask: Int, value: Int) {
 
 }
 
 fun setBits(mask: Int){
-    val write = mask.or(UsbPort.read())
+    val write = mask.or(output)
     UsbPort.write(write)
+    output = write
 }
+
+// Coloca os bits representados por mask no valor lógico ‘0’
+fun clrBits(mask: Int) {
+    val write = (mask.xor(output))
+    UsbPort.write(write)
+    output = write
+}
+
