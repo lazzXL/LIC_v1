ec69a57a9f49644e9dbd8ebe0e8e912500b2f136
lazzXL <gabs8b@gmail.com>
2023-05-22 20:21:17 +0100

    User v1

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,5 +1,17 @@
-object User {
-    fun newUser(userName: String, pin: Int) {
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
+object User {
+    private val userFile = FileAccess.openFile("users.txt")
+    private var userArray = FileAccess.readFile(userFile)
+    fun newUser(userName: String, pin: Int) {
+        if (!FileAccess.openFile("users.txt").exists()) {
+            FileAccess.createFile("users.txt")
+        }
+        val newUserIN = userArray.last().split("|")[0] + 1
+        FileAccess.writeFile(userFile, "| $newUserIN | $userName | $pin")
     }
+    fun removeUser(UIN: Int) = FileAccess.editFile(userFile, UIN, "User $UIN removed")
+
+
 }
